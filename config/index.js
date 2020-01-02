d5a7899a558c8e840c4d590fed38be0584e1b951
yujihu <yujihu@jd.com>
2020-01-02 18:43:54 +0800

    feat: update token

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 module.exports.baseURL = 'https://api.github.com';
 module.exports.orgName = 'YolkPie';
-module.exports.token = '3b03959a15e3ae8c71ed1b09be9c2bf0d7cefc1e';
+module.exports.token = 'eb1872f2840a826cb954a7e11a46254a9d17f65b';
