d6211b5d0767f6705b00a1bdb78e3ac896f61081
yujihu <yujihu@jd.com>
2020-01-10 11:01:46 +0800

    feat: token加密

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 module.exports.baseURL = 'https://api.github.com';
 module.exports.orgName = 'YolkPie';
-module.exports.token = 'a407f651153a4e403807787686178a80e194bdbb';
+module.exports.token = Buffer.from('NWVhNTJmZDM2MDdjNTQ2Y2RjZGEwNWE4N2UwNzUyZTRhMGI1NzA1Mw==', 'base64').toString();
